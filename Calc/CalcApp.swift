f81ec28fd9d124662b8dedf8c4ac62716438af19
pratama6624 <scyoung6624@gmail.com>
2024-11-09 19:56:02 +0700

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by Pratama One on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calc
+//
+//  Created by Pratama One on 09/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
