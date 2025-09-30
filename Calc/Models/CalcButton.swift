5c2935fd5a02edd800ca86f941e4ee62fc6c2eda
pratama6624 <scyoung6624@gmail.com>
2025-09-30 08:04:03 +0700

    Add Button Model

---
+++ Calc/Models/CalcButton.swift
@@ -0,0 +1,29 @@
+//
+//  CalcButton.swift
+//  Calc
+//
+//  Created by Pratama One on 30/09/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum CalcButton: String {
+    case zero = "0", one = "1", two = "2", three = "3"
+    case four = "4", five = "5", six = "6"
+    case seven = "7", eight = "8", nine = "9"
+    case add = "+", subtract = "-", multiply = "x", divide = "÷"
+    case equal = "=", clear = "C", decimal = ".", percent = "%"
+    case delete = "⌫", parentheses = "()"
+    
+    var backgroundColor: Color {
+        switch self {
+            case .add, .subtract, .multiply, .divide, .equal:
+                return Color.pink.opacity(0.8)
+            case .clear, .delete, .percent, .parentheses:
+                return Color.gray.opacity(0.2)
+            default:
+                return Color.white
+        }
+    }
+}
---
+++ Calc/sample.swift
@@ -0,0 +1,7 @@
+//
+//  sample.swift
+//  Calc
+//
+//  Created by Pratama One on 30/09/25.
+//
+
